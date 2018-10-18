98752bb59478da320bd55f191cc54099683b3157
Leonardo Costa <leocosta@edu.unifor.br>
2018-10-18 10:38:37 -0300

    começando testes

--- node_I/config/express.js
+++ node_I/config/express.js
@@ -7,6 +7,7 @@ const expressValidator = require('express-validator');
 module.exports = () => {
     const app = express();
 
+    
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
--- node_I/test/produtosTest.js
+++ node_I/test/produtosTest.js
@@ -1,6 +1,22 @@
+const http = require('http');
+const assert = require('assert');
 describe('#ProdutosController', () => {
-    it('#listagem json',() => {
-        console.log('teste de verificacao da listagem json');
+    it('#listagem json',(done) => {
+        const config = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        };
+
+        http.get(config, (res) => {
+            assert.equal(res.statusCode,200);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+            done();
+        });
+
         
     });
 });
